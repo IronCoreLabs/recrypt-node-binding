a4a1060391bb198a84492d197e6efc301769e596
Clint Frederickson <clint.frederickson@ironcorelabs.com>
2020-02-20 16:07:18 -0700

    [#38] try ./node_modules again

--- publish.js
+++ publish.js
@@ -52,7 +52,7 @@ fs.writeFileSync("./dist/package.json", JSON.stringify(npmPackageJson, null, 2))
 
 shell.mkdir("./bin-package");
 shell.cp("./native/index.node", "./bin-package");
-shell.exec("node_modules/.bin/node-pre-gyp package");
+shell.exec("./node_modules/.bin/node-pre-gyp package");
 var tgz = shell.exec("find ./build -name *.tar.gz");
 shell.cp(tgz, "./bin-package/");
 shell.pushd("./dist");
