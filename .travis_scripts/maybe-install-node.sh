03ca505af1556dac73d30057beb98e227f684585
Clint Frederickson <clint.frederickson@ironcorelabs.com>
2020-02-20 10:50:07 -0700

    [#38] move nvs into maybe-install-node.sh and disable non windows build jobs for now

--- .travis_scripts/maybe-install-node.sh
+++ .travis_scripts/maybe-install-node.sh
@@ -3,10 +3,18 @@
 # Installs correct node and yarn versions if we aren't doing a docker build.
 
 if [ -z "${IMAGE}" ] ; then
+
+	git clone --branch v$NVS_VERSION --depth 1 https://github.com/jasongin/nvs ~/.nvs
+	. ~/.nvs/nvs.sh
+	nvs --version
+	nvs add $NODEJS_VERSION
+	nvs use $NODEJS_VERSION
+	node --version
+	
     # install our own yarn to make things work on osx
     curl -o- -L https://yarnpkg.com/install.sh | bash -s -- --version 1.22.0
     export PATH=$HOME/.yarn/bin:$PATH
 
     # install our own nodejs to get a reasonable version if outside docker
-    rm -rf ~/.nvm && git clone https://github.com/creationix/nvm.git ~/.nvm && (cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`) && source ~/.nvm/nvm.sh && nvm install $TRAVIS_NODE_VERSION
+    # rm -rf ~/.nvm && git clone https://github.com/creationix/nvm.git ~/.nvm && (cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`) && source ~/.nvm/nvm.sh && nvm install $TRAVIS_NODE_VERSION
 fi
