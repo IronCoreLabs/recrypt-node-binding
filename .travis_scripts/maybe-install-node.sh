a6c82fc5b5f01fcea933e623a46cb9337d9b6a4d
Clint Frederickson <clint.frederickson@ironcorelabs.com>
2020-02-20 11:14:57 -0700

    [#38] use nvs 1.5.4

--- .travis_scripts/maybe-install-node.sh
+++ .travis_scripts/maybe-install-node.sh
@@ -4,13 +4,13 @@
 
 if [ -z "${IMAGE}" ] ; then
 
-	git clone --branch v$NVS_VERSION --depth 1 https://github.com/jasongin/nvs ~/.nvs
+	git clone --branch v1.5.4 --depth 1 https://github.com/jasongin/nvs ~/.nvs
 	. ~/.nvs/nvs.sh
 	nvs --version
 	nvs add $NODEJS_VERSION
 	nvs use $NODEJS_VERSION
 	node --version
-	
+
     # install our own yarn to make things work on osx
     curl -o- -L https://yarnpkg.com/install.sh | bash -s -- --version 1.22.0
     export PATH=$HOME/.yarn/bin:$PATH
