f4ec743fcae404377a2b7a0397b36c87dd8dc1e3
Ernie Turner <ernie.turner@ironcorelabs.com>
2020-02-20 17:25:06 -0700

    Use fully qualified path to pre-gyp binary

--- publish.js
+++ publish.js
@@ -52,7 +52,10 @@ fs.writeFileSync("./dist/package.json", JSON.stringify(npmPackageJson, null, 2))
 
 shell.mkdir("./bin-package");
 shell.cp("./native/index.node", "./bin-package");
-shell.exec("./node_modules/.bin/node-pre-gyp package");
+//Use a fully qualified path to pre-gyp binary for Windows support
+const cwd = shell.pwd().toString();
+shell.exec(`${cwd}/node_modules/node-pre-gyp/bin/node-pre-gyp package`);
+//shell.exec("./node_modules/.bin/node-pre-gyp package");
 var tgz = shell.exec("find ./build -name *.tar.gz");
 shell.cp(tgz, "./bin-package/");
 shell.pushd("./dist");
