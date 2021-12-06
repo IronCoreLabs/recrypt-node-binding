f01c2a57a75b3dc7d43cb3121ba6f3106260bb67
Murph Murphy <murph@clurictec.com>
2021-12-06 12:43:48 -0700

    Remove TODOs

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 "use strict";
 
-// TODO: is this even going to resolve when distributed?
-// TODO: might want to introduce a TS build process and generate the index.d.ts
 const internal = require("./bin-package/index.node");
 
 class Api256 {
