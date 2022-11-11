2d65b6050b1630d080ada96a99ab470fc5ca8ce3
Colt Frederickson <coltfred@gmail.com>
2022-11-11 16:44:20 -0700

    Fix benchmarks and flake.nix

--- benchmark/decryptLevelTwo.js
+++ benchmark/decryptLevelTwo.js
@@ -1,6 +1,5 @@
 const Benchmark = require("benchmark");
-const recrypt = require("../index.node");
-//Randomly generated legit ED25519 keypair
+const recrypt = require("../index.js");//Randomly generated legit ED25519 keypair
 const privateSigningKey = Buffer.from("O7f2FYsabKOFj3enK+HQ+cBmTMbAG6aCesd1nLcFM1wtA9XHg0+rFIVA7+nomADjEbJ1R/Gd+xHBO79UnLqxDQ==", "base64");
 
 const api = new recrypt.Api256();
--- flake.nix
+++ flake.nix
@@ -1,34 +1,26 @@
 {
   description = "Recrypt Node Binding";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, rust-overlay, flake-utils}:
     flake-utils.lib.eachDefaultSystem (system:
       let
         lib = import <nixpkgs/lib>;
-        pkgs = nixpkgs.legacyPackages.${system};
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
       in
       rec {
-        packages = {
-          recrypt-node-binding = {
-            name = "recrypt-node-binding";
-            version = "0.1.0";
-            src = ./.;
-            buildInputs = with pkgs.nodePackages; [
-              pkgs.nodejs-18_x
-              (pkgs.yarn.override { nodejs = nodejs-18_x; })
-            ];
-            nativeBuildInputs = [ ]
-              ++ lib.optionals (pkgs.stdenv.isDarwin) [ pkgs.darwin.cctools ];
-          };
-        };
-        defaultPackage = packages.recrypt-node-binding;
-
         devShell = pkgs.mkShell {
           buildInputs = with pkgs.nodePackages; [
             pkgs.nodejs-18_x
             (pkgs.yarn.override { nodejs = pkgs.nodejs-18_x; })
+            pkgs.libiconv
           ];
+          nativeBuildInputs = [ ]
+              ++ pkgs.lib.optionals (pkgs.stdenv.isDarwin) [ pkgs.darwin.cctools ];
         };
       });
 }
